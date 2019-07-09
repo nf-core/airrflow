9b0ced792fb770ce178a9577cffbd975b58fd2ed
ggabernet <gisela.gabernet@qbic.uni-tuebingen.de>
2019-07-09 17:29:15 +0200

    debug fonts

--- bin/alakazam.R
+++ bin/alakazam.R
@@ -11,9 +11,9 @@ library("svglite")
 library("extrafont")
 
 str(pdfFonts(), max.level=1)
-extrafont::font_import(prompt=FALSE)
 
-extrafont::loadfonts()
+
+
 theme_set(theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()))
 
 
