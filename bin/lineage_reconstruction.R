3bf06d2dabbea87d63fa19dcc5e57e669d8ed489
ggabernet <gisela.gabernet@qbic.uni-tuebingen.de>
2020-09-18 17:39:18 +0200

    lineage reconstruction > 2 seqs

--- bin/lineage_reconstruction.R
+++ bin/lineage_reconstruction.R
@@ -38,7 +38,7 @@ countclones <- countClones(df_pat,clone="CLONE", copy="DUPCOUNT")
 write.table(countclones, paste(patdir_lineage, "/", "Clones_table_patient_", df_pat$SOURCE[1],".tsv", sep=""), quote=F, sep="\t", row.names = F)
 
 # Restrict clonal tree size
-clones <- subset(countclones, SEQ_COUNT >= 2 & SEQ_COUNT < 1000)
+clones <- subset(countclones, SEQ_COUNT > 2 & SEQ_COUNT < 1000)
 
 dnapars_exec_tab <- read.csv("dnapars_exec.txt", header=F)
 dnapars_exec <- as.character(dnapars_exec_tab[1,1])
