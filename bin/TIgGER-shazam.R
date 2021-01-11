6ed0f0598025764c8e2bb9f0300fefa56b2702f2
ggabernet <gisela.gabernet@qbic.uni-tuebingen.de>
2021-01-11 15:15:15 +0100

    fix dist tr

--- bin/TIgGER-shazam.R
+++ bin/TIgGER-shazam.R
@@ -68,6 +68,7 @@ if (loci == "ig"){
   # Did not work with "V_CALL_GENOTYPED" for tr loci
   dist_ham <- distToNearest(db, vCallColumn="V_CALL", model="ham", 
                           normalize="len", nproc=1, first = TRUE)
+  print(dist_ham)
 } else {
   stop("Loci specified is not available, please choose from: ig, tr.")
 }
