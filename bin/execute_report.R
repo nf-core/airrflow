4a38c1ccb0a84e74fe5aaaa9e3106ffe9b0742f1
ggabernet <gisela.gabernet@qbic.uni-tuebingen.de>
2022-04-26 13:33:23 +0200

    test fix rmarkdown aws

--- bin/execute_report.R
+++ bin/execute_report.R
@@ -1,9 +1,21 @@
 #!/usr/bin/env Rscript
 
 library(rmarkdown)
+library(optparse)
 
-args = commandArgs(trailingOnly=TRUE)
+option_list = list(
+    make_option(c("-r", "--report_file"), type="character", default=NULL, help="report rmarkdown file", metavar="character"),
+    make_option(c("-rf", "--references"), type="character", default=NULL, help="references bibtex file", metavar="character"),
+    make_option(c("-c","--css"), type ="character", default=NULL, help="css file for report formatting", metavar="character"),
+    make_option(c("-l","--logo"), type="character", default=NULL, help="logo for report", metavar="character")
+)
+
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
 
 wd=getwd()
 
-rmarkdown::render(args[1], output_file = "Bcellmagic_report.html", knit_root_dir = wd, output_dir = wd)
+rmarkdown::render(opt$report_file, output_file = "Bcellmagic_report.html", knit_root_dir = wd, output_dir = wd,
+                    params = list(path_references = opt$references,
+                                    path_css = opt$css,
+                                    path_logo = opt$logo))
