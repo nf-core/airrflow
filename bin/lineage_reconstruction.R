c07ce1b2a21fcfcfef6b234e5a9eaf53752ff799
ggabernet <gisela.gabernet@qbic.uni-tuebingen.de>
2022-07-15 10:32:56 +0200

    fix lineage reconstruction

--- bin/lineage_reconstruction.R
+++ bin/lineage_reconstruction.R
@@ -56,10 +56,9 @@ dnapars_exec <- as.character(dnapars_exec_tab[1,1])
 # Create clonal tree per clone
 save_graph <- function(df_pat, clone_num){
     print(paste0("Started processing clone:",clone_num))
-    sub_db_clone <- subset(df_pat, clone_id == clone_num)
-    sub_db_clone <- data.frame(sapply(sub_db_clone, as.character), stringsAsFactors=FALSE)
-    sub_db_clone$duplicate_count <- sapply(sub_db_clone$duplicate_count, as.numeric)
-    sub_db_clone$junc_len <- sapply(sub_db_clone$junc_len, as.numeric)
+    sub_db_clone = dplyr::filter(df_pat, clone_id == clone_num)
+                                %>% dplyr::mutate(across(everything()),as.character)
+                                %>% dplyr::mutate(across(c(junc_len,duplicate_count)), as.numeric)
 
     # Make changeo clone and Build Phylip Lineage
     if ( node_text == "none" ) {
